6a1bcd81d8051eeeba80985292927fabf09d3a50
shrishtinigam <mehershrishtinigam@gmail.com>
2021-09-28 23:41:59 +0530

    index.html updated, main.css updated, main.js added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 var clip = new Clipboard('.btn');
+
+clip.on("success", function() {
+  document.body.insertAdjacentHTML('beforeend', '<div></div>');
+});
+clip.on("error", function() {
+  document.body.insertAdjacentHTML('beforeend', '<div></div>');
+});
